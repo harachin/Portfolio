84afd03504b74b5bf0ab320c070032106ab707b9
MasatoHarashinai <harashinai.m@gmail.com>
2022-02-09 03:47:14 +0900

    カードへリンク埋め込み

--- src/views/Works.vue
+++ src/views/Works.vue
@@ -9,6 +9,7 @@
       :preview="work.icon"
       :name="work.name"
       :desc="work.desc"
+      @click="onTapCard(work.url)"
     />
   </div>
   <Heading title="Web services" />
@@ -20,6 +21,7 @@
       :preview="work.icon"
       :name="work.name"
       :desc="work.desc"
+      @click="onTapCard(work.url)"
     />
   </div>
 </template>
@@ -39,6 +41,7 @@ interface WorkInformation {
   name: string;
   desc: string;
   icon: string;
+  url: string;
 }
 @Options({
   components: {
@@ -55,20 +58,26 @@ export default class Works extends Vue {
       name: "DROID I",
       desc: "学生時代、文化祭にて一般公開用に開発した対戦型2Dシューティングゲームです。",
       icon: require("@/assets/work_screenshot_droid_i_1.png"),
+      url: "https://github.com/harachin/DROID_I",
     },
     {
       category: WorkCategory.GAME,
       name: "DROID II",
       desc: "卒業制作にて開発した縦型スクロールシューティングゲームです。敵を撃破する毎に経験値を獲得することができシールドやボムなどの追加装備を獲得できる。",
       icon: require("@/assets/work_screenshot_droid_ii_1.png"),
+      url: "https://github.com/harachin/DROID_II",
     },
     {
       category: WorkCategory.WEB_SERVICE,
       name: "Harachin.lab",
       desc: "エンジニアHarachinのポートフォリオサイトです。",
       icon: require("@/assets/work_screenshot_harachinlab_1.png"),
+      url: "https://github.com/harachin/Portfolio",
     },
   ];
+  onTapCard(url: string) {
+    window.open(url, "_blank");
+  }
 
   get games(): Array<WorkInformation> {
     return this.categoryOf(WorkCategory.GAME);
